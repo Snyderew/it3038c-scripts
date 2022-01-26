7dacd411813a33b5309436eaf2497ffa7e06ffa3
uc-snyderew <snyderew@mail.uc.edu>
2022-01-25 22:08:42 -0500

    sendemail.ps1

---
+++ powershell/sendemail.ps1
@@ -0,0 +1,25 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+
+$User = $env:Username
+
+$CN = $env:ComputerName
+
+function getVer{
+    ($HOST.version).Major
+}
+
+$ver = getver
+
+function getDMY{
+    get-date -format "dddd, MMMM dd, yyyy"
+}
+
+$DMY = getDMY
+
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $CN. PowerShell $ver. Today's Date is $DMY."
+
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "edsnyder99@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
