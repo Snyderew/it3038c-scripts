a3e767c86bca2b1f6add1da76cd99e9c58bdfe94
uc-snyderew <snyderew@mail.uc.edu>
2022-03-07 16:43:21 -0500

    lab 7 windows commit

--- labs/lab7.py
+++ labs/lab7.py
@@ -1,4 +1,4 @@
-from /usr/local/lib/python3.6/site-packages/TextStatistic import count_words
+from TextStatistic import count_words
 
 Print("Enter the name of the file you want to get a word count from.")
 
